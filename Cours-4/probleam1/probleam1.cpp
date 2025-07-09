9bd88131188d763e06fdc8293735cc7fb7358dc1
Valkenan <adamfennych07@gmail.com>
2025-07-09 17:47:44 +0100

    cours4

---
+++ Cours-4/probleam1/probleam1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+void PrintName(string name)
+{
+	cout << "\n your name is : " << name << endl;
+}
+int main()
+{
+	PrintName("adam");
+	return 0;
+}
+
